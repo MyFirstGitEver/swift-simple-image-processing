25fd3cdb22f0ba371bcf2a8faa177e9da9dfa73d
DUC <fordevok@gmail.com>
2023-07-13 22:50:01 -0700

    sample images I've tested on

--- swift-image-processing/main.swift
+++ swift-image-processing/main.swift
@@ -11,11 +11,14 @@ let fileManager = FileManager.default
 let home = fileManager.homeDirectoryForCurrentUser.appendingPathComponent("Desktop/Images")
 let fileURL = home.appendingPathComponent("test.png")
 
-try ImageProcessor(fileURL).compressUsingKmeans(k: 60, effort: 4)?
-    .save(home.appendingPathComponent("compressed.png"))
+//try ImageProcessor(fileURL).compressUsingKmeans(k: 60, effort: 4)?
+//    .save(home.appendingPathComponent("compressed.png"))
 
 //try ImageProcessor(home.appendingPathComponent("zelda.png")).draftThisImage()?
 //    .save(home.appendingPathComponent("contour.png"))
+
+try ImageProcessor(home.appendingPathComponent("gray.jpg")).draftThisImage()?
+    .save(home.appendingPathComponent("contour2.png"))
 
 //try ImageProcessor(fileURL).rotateRight()?.save(home.appendingPathComponent("right.png"))
 //try ImageProcessor(fileURL).rotateLeft()?.save(home.appendingPathComponent("left.png"))
